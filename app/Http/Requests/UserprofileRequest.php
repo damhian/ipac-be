9c9450b7f6de21569f35ab879fde5286809adcfd
David Mayshiogie <david.mayshiogie@protonmail.com>
2023-07-22 10:24:38 +0700

    add user profiles

---
+++ app/Http/Requests/UserprofileRequest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'license_number' => 'required|integer',
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'tahun_masuk' => 'required|integer',
+            'tahun_lulus' => 'required|integer',
+            'training_program' => 'required|string|max:80',
+            'batch' => 'required|string|max:25',
+            'current_job' => 'required|in:121,135,91,141,TNI,Polri,PNS,Pensiunan,Belum bekerja',
+            'current_workplace' => 'required|string|max:255',
+            'birth_place' => 'required|string|max:80',
+            'date_of_birth' => 'required|date|date_format:Y-m-d',
+            'address' => 'nullable|string|max:255',
+            'phone_number' => 'required|string|max:15',
+            'phone_number_code' => 'required|string|max:4',
+            'gender' => 'required|string|max:12',
+        ];
+    }
+
+    /**
+     * Get the validation error messages.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'license_number.required' => 'The license number field is required.',
+            'license_number.integer' => 'The license number must be an integer.',
+            'first_name.required' => 'The first name field is required.',
+            'first_name.max' => 'The first name may not be greater than :max characters.',
+            'last_name.required' => 'The last name field is required.',
+            'last_name.max' => 'The last name may not be greater than :max characters.',
+            'tahun_masuk.required' => 'The tahun masuk field is required.',
+            'tahun_masuk.integer' => 'The tahun masuk must be an integer.',
+            'tahun_lulus.required' => 'The tahun lulus field is required.',
+            'tahun_lulus.integer' => 'The tahun lulus must be an integer.',
+            'training_program.required' => 'The training program field is required.',
+            'training_program.max' => 'The training program may not be greater than :max characters.',
+            'batch.required' => 'The batch field is required.',
+            'batch.max' => 'The batch may not be greater than :max characters.',
+            'current_job.required' => 'The current job field is required.',
+            'current_job.in' => 'The selected current job is invalid.',
+            'current_workplace.required' => 'The current workplace field is required.',
+            'current_workplace.max' => 'The current workplace may not be greater than :max characters.',
+            'birth_place.required' => 'The birth place field is required.',
+            'birth_place.max' => 'The birth place may not be greater than :max characters.',
+            'date_of_birth.required' => 'The date of birth field is required.',
+            'date_of_birth.date' => 'The date of birth must be a valid date with this format: Y-m-d!',
+            'address.max' => 'The address may not be greater than :max characters.',
+            'phone_number.required' => 'The phone number field is required.',
+            'phone_number.max' => 'The phone number may not be greater than :max characters.',
+            'phone_number_code.required' => 'The phone number code field is required.',
+            'phone_number_code.max' => 'The phone number code may not be greater than :max characters.',
+            'gender.required' => 'The gender field is required.',
+            'gender.max' => 'The gender may not be greater than :max characters.',
+        ];
+    }
+}
---
+++ app/Models/Userprofiles.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Usergallery;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class UserProfiles extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'alumni_id',
+        'nomor_anggota',
+        'license_number',
+        'profile_image_id',
+        'first_name',
+        'last_name',
+        'tahun_masuk',
+        'tahun_lulus',
+        'training_program',
+        'batch',
+        'current_job',
+        'current_workplace',
+        'birth_place',
+        'date_of_birth',
+        'address',
+        'phone_number',
+        'phone_number_code',
+        'gender',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * Get the user associated with the user profile.
+     */
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'alumni_id');
+    }
+
+    /**
+     * Get the user gallery associated with the user profile.
+     */
+    public function userGallery(): HasOne
+    {
+        return $this->hasOne(Usergallery::class, 'alumni_id', 'alumni_id');
+    }
+}
