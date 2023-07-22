653e097c16c5487e6fc2540c27a15c55bf7344bd
David Mayshiogie <david.mayshiogie@protonmail.com>
2023-07-22 18:37:37 +0700

    add more functions

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,7 +14,12 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+         // Fetch all users with their related data
+         $users = User::with('userExperience', 'userProfiles', 'userGallery')->get();
+
+         return response()->json([
+             'users' => $users
+         ], 200);
     }
 
     /**
@@ -48,6 +54,48 @@ class UserController extends Controller
         return response()->json([
             'user' => $user
         ], 200);
+    }
+
+    public function showbytoken()
+    {
+        $user = Auth::user();
+
+        $user->load('userExperience', 'userProfiles', 'userGallery');
+
+        return response()->json([
+            'user' => $user
+        ], 200);
+    }
+
+    public function showUserbyTahunLulus(Request $request)
+    {
+        $tahunLulus = $request->input('tahun_lulus', null);
+
+        $usersWithProfilesCount = $this->countUsersWithTahunLulus($tahunLulus);
+
+        return response()->json([
+            'User' => $usersWithProfilesCount
+        ]);
+
+    }
+
+    private function countUsersWithTahunLulus($tahunLulus = null)
+    {
+        $query = User::query();
+
+        // If 'tahun_lulus' is provided, filter users based on 'tahun_lulus'
+        if ($tahunLulus !== null) {
+            $query->whereHas('userProfiles', function ($query) use ($tahunLulus) {
+                $query->where('tahun_lulus', $tahunLulus);
+            });
+        }
+
+        // Exclude users with null 'tahun_lulus'
+        $query->whereHas('userProfiles', function ($query) {
+            $query->whereNotNull('tahun_lulus');
+        });
+
+        return $query->count();
     }
 
     /**
