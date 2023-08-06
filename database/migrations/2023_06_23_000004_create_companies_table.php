f719d00a96302fb8eed7e8f8f5050c5fb795c85a
David Mayshiogie <david.mayshiogie@protonmail.com>
2023-08-06 12:12:58 +0700

    add reference to user

--- database/migrations/2023_06_23_000004_create_companies_table.php
+++ database/migrations/2023_06_23_000004_create_companies_table.php
@@ -13,8 +13,11 @@ class CreateCompaniesTable extends Migration
             $table->string('image_url', 255)->nullable();
             $table->string('name', 50);
             $table->text('about')->nullable();
+            $table->unsignedBigInteger('created_by');
             $table->dateTime('created_at');
             $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('created_by')->references('id')->on('users');
         });
     }
 
