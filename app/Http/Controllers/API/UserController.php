1f1d95a2dbb48897f7a2fbdc43cf6fba1f8e38d4
David Mayshiogie <david.mayshiogie@protonmail.com>
2023-08-06 12:07:53 +0700

    add function to add user & update user capability

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     {
          // Fetch all users with their related data
          $users = User::with('userExperience', 'userProfiles', 'userGallery')->get();
-
+         
          return response()->json([
              'users' => $users
          ], 200);
@@ -35,7 +35,38 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'username' => 'required|unique:users',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6',
+                'role' => 'in:superadmin,admin,alumni', // Assuming 'role' can be either 'admin' or 'alumni'
+                // 'status' => 'in:approved,pending', // Assuming 'status' can be either 'approved' or 'pending'
+            ]);
+    
+            // Create the new user
+            $user = User::create([
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => bcrypt($request->password), // Hash the password
+                'role' => $request->role,
+                // 'status' => $request->status,
+            ]);
+    
+            // Optionally, you can also create related models like user profiles, experiences, etc., if needed.
+    
+            return response()->json([
+                'message' => 'User created successfully',
+                'user' => $user
+            ], 201);
+    
+        } catch (\Throwable $th) {
+            // Return error response in case of an exception
+            return response()->json([
+                'message' => 'Failed to create user',
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -59,11 +90,16 @@ class UserController extends Controller
     public function showbytoken()
     {
         $user = Auth::user();
+        
+        $user->userProfiles;
+        $user->userGallery;
+        $user->userExperience;
 
-        $user->load('userExperience', 'userProfiles', 'userGallery');
+        // Old method
+        // $user->load('userExperience', 'userProfiles', 'userGallery');
 
         return response()->json([
-            'user' => $user
+            'user' => $user,
         ], 200);
     }
 
@@ -111,7 +147,50 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $request->validate([
+                'username' => 'required|unique:users,username,' . $id,
+                'email' => 'required|email|unique:users,email,' . $id,
+                'password' => 'sometimes|min:6', // Only validate if 'password' is provided in the request
+                'role' => 'in:admin,alumni',
+                // 'status' => 'in:approved,pending',
+            ]);
+    
+            // Find the user by ID
+            $user = User::find($id);
+    
+            if (!$user) {
+                return response()->json([
+                    'message' => 'User not found'
+                ], 404);
+            }
+    
+            // Update the user data
+            $user->username = $request->username;
+            $user->email = $request->email;
+            $user->role = $request->role;
+            // $user->status = $request->status;
+    
+            // Update the password if provided
+            if ($request->has('password')) {
+                $user->password = bcrypt($request->password);
+            }
+    
+            // Save the changes
+            $user->save();
+    
+            return response()->json([
+                'message' => 'User updated successfully',
+                'user' => $user
+            ], 200);
+    
+        } catch (\Throwable $th) {
+            // Return error response in case of an exception
+            return response()->json([
+                'message' => 'Failed to update user',
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
