cf39342c6838c3105529c972da719572d27c86de
damhian <david.mayshiogie@protonmail.com>
2023-08-19 18:20:11 +0700

    add superadmin phoneNumber

--- app/Http/Controllers/API/StoreController.php
+++ app/Http/Controllers/API/StoreController.php
@@ -6,37 +6,45 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRequest;
 use App\Models\Store;
 use App\Models\Storemedia;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-
     public function __construct()
     {
         $this->middleware('admin')->only('updateStatus');
     }
 
-    public function index()
-    {
+    public function index(Request $request)
+    {  
         $stores = Store::with(['storeMedia', 'user.userProfiles'])
-                        ->where('status', '!=', 'deleted')
-                        ->get();
+                    ->where('status', '=', 'approved')
+                    ->where('status', '!=', 'deleted')
+                    ->get();
 
+        $superadminPhoneNumber = null;
+
+        $superadmin = User::where('role', 'superadmin')
+            ->with('userProfiles:alumni_id,phone_number_code,phone_number')
+            ->first();
+
+        if ($superadmin) {
+            $superadminPhoneNumber = $superadmin->userProfiles->phone_number_code .''. $superadmin->userProfiles->phone_number;
+        }
+                    
+        foreach ($stores as $store) {
+            $store->superadminPhone = $superadminPhoneNumber;
+        }
+                
         return response()->json([
             'stores' => $stores
         ], 200);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -95,16 +103,30 @@ class StoreController extends Controller
     public function show(string $id)
     {
         // Get Store data by id
-        $store = Store::with('storeMedia')->find($id);
+        $store = Store::with(['storeMedia', 'user.userProfiles'])->find($id);
 
         if(!$store)
             return response()->json([
                 'message' => 'store not found!'
             ], 404);
+
+            // Retrieve superadminPhoneNumber
+        $superadminPhoneNumber = null;
+
+        $superadmin = User::where('role', 'superadmin')
+            ->with('userProfiles:alumni_id,phone_number_code,phone_number')
+            ->first();
+
+        if ($superadmin) {
+            $superadminPhoneNumber = $superadmin->userProfiles->phone_number_code .''. $superadmin->userProfiles->phone_number;
+        }
+
+        // Add superadminPhone to the store entry
+        $store->superadminPhone = $superadminPhoneNumber;
         
         // Return response success
         return response()->json([
-            'Store' => $store
+            'store' => $store
         ], 200);
     }
 
@@ -114,12 +136,34 @@ class StoreController extends Controller
         $user = Auth::user();
         
         // Find the store associated with the token
-        $store = Store::with('storeMedia')->where('created_by', $user->id)->get();
+        if ($user->isAdmin()) {
+            $query = Store::with(['storeMedia', 'user.userProfiles']);
+        } else {
+            $query = Store::with(['storeMedia', 'user.userProfiles'])
+            ->where('created_by', $user->id);
+        }
+
+        $store = $query->get();
 
         if (!$store) {
             return response()->json([
                 'message' => 'User profile not found!'
             ], 404);
+        }
+
+        // Retrieve superadminPhoneNumber
+        $superadminPhoneNumber = null;
+        $superadmin = User::where('role', 'superadmin')
+            ->with('userProfiles:alumni_id,phone_number_code,phone_number')
+            ->first();
+
+        if ($superadmin) {
+            $superadminPhoneNumber = $superadmin->userProfiles->phone_number_code .''. $superadmin->userProfiles->phone_number;
+        }
+
+        // Add superadminPhone to each store entry in the response
+        foreach ($store as $storeEntry) {
+            $storeEntry->superadminPhone = $superadminPhoneNumber;
         }
 
         // Return response success
@@ -134,7 +178,7 @@ class StoreController extends Controller
         // $user = Auth::user();
         
         // Find the store associated with the user id from their login
-        $store = Store::with('storeMedia')->where('created_by', $id)->get();
+        $store = Store::with(['storeMedia', 'user.userProfiles'])->where('created_by', $id)->get();
 
         if (!$store) {
             return response()->json([
@@ -142,18 +186,25 @@ class StoreController extends Controller
             ], 404);
         }
 
+        // Retrieve superadminPhoneNumber
+        $superadminPhoneNumber = null;
+        $superadmin = User::where('role', 'superadmin')
+            ->with('userProfiles:alumni_id,phone_number_code,phone_number')
+            ->first();
+
+        if ($superadmin) {
+            $superadminPhoneNumber = $superadmin->userProfiles->phone_number_code .''. $superadmin->userProfiles->phone_number;
+        }
+
+        // Add superadminPhone to each store entry in the response
+        foreach ($store as $storeEntry) {
+            $storeEntry->superadminPhone = $superadminPhoneNumber;
+        }
+
         // Return response success
         return response()->json([
             'stores' => $store
         ], 200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
     }
 
     /**
@@ -169,27 +220,37 @@ class StoreController extends Controller
                 return response()->json([
                     'message' => 'Store not found!'
                 ]);
-            
+
             $store->title = $request->title;
             $store->content = $request->content;
             $store->short_description = $request->short_description;
             $store->price = $request->price;
-            
-            // Update Media
-            if ($request->hasFile('images')) {
-                $storeMedia = [];
 
+            // Delete images that are not present in the new request
+            $newImageFileNames = [];
+            if ($request->hasFile('images')) {
+                $newImageFileNames = [];
                 foreach ($request->file('images') as $image) {
                     $filePath = $image->store('store_media', 'public');
+                    $newImageFileNames[] = $filePath;
+                }
+            }
 
+            // Delete existing media that are not present in the new images
+            $store->storeMedia()
+                ->whereNotIn('filename', $newImageFileNames)
+                ->delete();
+
+            // Save new images
+            if ($request->hasFile('images')) {
+                $storeMedia = [];
+                foreach ($newImageFileNames as $fileName) {
                     $storeMedia[] = new StoreMedia([
-                        'filename' => $filePath,
+                        'filename' => $fileName,
                         'created_by' => Auth::id()
                     ]);
                 }
-
-                $store->storeMedia()->delete(); // Delete existing media
-                $store->storeMedia()->saveMany($storeMedia); // Save new media
+                $store->storeMedia()->saveMany($storeMedia);
             }
 
             $store->save();
@@ -202,8 +263,8 @@ class StoreController extends Controller
 
         } catch (\Throwable $th) {
             DB::rollBack();
-             // return json response
-             return response()->json([
+            // return json response
+            return response()->json([
                 'message' => 'something went wrong!',
                 'error message' => $th
             ], 500);
