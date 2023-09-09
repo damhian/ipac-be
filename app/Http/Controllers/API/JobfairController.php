14fff35d9b60a4e3647ceb744fc19c51728406db
damhian <david.mayshiogie@protonmail.com>
2023-09-09 22:57:25 +0700

    add filtering

--- app/Http/Controllers/API/JobfairController.php
+++ app/Http/Controllers/API/JobfairController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\JobfairRequest;
 use App\Models\Jobfair;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -87,7 +88,7 @@ class JobfairController extends Controller
         ], 200);
     }
 
-    public function showByToken()
+    public function showByToken(Request $request)
     {
         // Get the authenticated user's token
         $user = Auth::user();
@@ -99,11 +100,85 @@ class JobfairController extends Controller
                     ->where('created_by', $user->id);
         }
 
+        // Apply filters
+        if ($request->has('title')) {
+            $query->where('title', 'like', '%' . $request->title . '%');
+        }
+
+        if ($request->has('content')) {
+            $query->where('content', 'like', '%' . $request->content . '%');
+        }
+
+        if ($request->has('short_description')) {
+            $query->where('short_description', 'like', '%' . $request->short_description . '%');
+        }
+
+        if ($request->has('region')) {
+            $query->where('region', 'like', '%' . $request->region . '%');
+        }
+
+        if ($request->has('company')) {
+            $query->where('company', 'like', '%' . $request->company . '%');
+        }
+
+        if ($request->has('jobtype')) {
+            $query->where('jobtype', 'like', '%' . $request->jobtype . '%');
+        }
+
+        if ($request->has('jobtitle')) {
+            $query->where('jobtitle', 'like', '%' . $request->jobtitle . '%');
+        }
+
+        if ($request->has('location_name')) {
+            $query->where('location_name', 'like', '%' . $request->location_name . '%');
+        }
+
+        if ($request->has('startAt')) {
+            // Convert the date string to a Carbon date object
+            $startDate = Carbon::parse($request->input('startAt'));
+
+            // Use the converted date in the query
+            $query->where('start_at', '=', $startDate);
+        }
+
+        if ($request->has('endAt')) {
+            $endDate = Carbon::parse($request->input('endAt'));
+            $query->where('end_at', '=', $endDate);
+        }
+
+        if ($request->has('status')) {
+            // Only add the "status" filter if the "status" input is provided
+            $query->where('status', $request->input('status'));
+        } else {
+            // If "status" input is not provided, exclude banners with status "deleted"   
+            $query->where('status', '=', 'approved')
+            ->where('status', '!=', 'deleted');
+        }
+
+        // Apply sorting
+        if ($request->has('sortBy')) {
+            $sortDirection = $request->input('sortDir', 'asc');
+            $sortBy = $request->input('sortBy');
+
+            // Validate the sort direction to prevent SQL injection
+            $validSortDirections = ['asc', 'desc'];
+
+            if (in_array($sortDirection, $validSortDirections) && in_array($sortBy, ['title', 'content', 'short_description', 'region', 'company', 'jobtype', 'jobtitle', 'location_name', 'start_at', 'end_at'])) {
+                $query->orderBy($sortBy, $sortDirection);
+            } else {
+                // Handle invalid sort parameters here (e.g., return an error response)
+                return response()->json([
+                    'message' => 'Invalid sort parameters provided.'
+                ], 400);
+            }
+        }
+
+        // Execute the query and get the results
         $jobfair = $query->get();
 
         if (!$jobfair) {
             return response()->json([
-                'message' => 'Store not found!'
+                'message' => 'Jobfair not found!'
             ], 404);
         }
 
