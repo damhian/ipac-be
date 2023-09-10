eaf49cc55d83212d1e6dee2c8b0e7a1d24b00743
damhian <david.mayshiogie@protonmail.com>
2023-09-11 00:58:47 +0700

    delete unused code

--- app/Http/Controllers/API/CompaniesController.php
+++ app/Http/Controllers/API/CompaniesController.php
@@ -26,14 +26,6 @@ class CompaniesController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(CompaniesRequest $request)
@@ -101,14 +93,6 @@ class CompaniesController extends Controller
         return response()->json([
             'Companies' => $companies
         ], 200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
     }
 
     /**
